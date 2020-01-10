022956c578a0a68deb1a1af58f045c10f0de8fd5
Felipe Livino <felipelivino@gmail.com>
2020-01-10 19:36:41 -0300

    inicio repo

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
+import { FaGitAlt, FaPlus } from "react-icons/fa";
 
-import { Title } from "./styles";
+import api from "../../services/api";
 
-function Main() {
-    return <Title>Olá mundo</Title>;
+import { Container, Form, SubmitButton } from "./styles";
+
+class Main extends Component {
+    state = {
+        newReppo: "",
+    };
+
+    handleInputChange = e => {
+        this.setState({ newReppo: e.target.value });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        console.log(this.state.newReppo);
+    };
+
+    render() {
+        const { newReppo } = this.state;
+
+        return (
+            <Container>
+                <h1>
+                    <FaGitAlt />
+                    Repositórios
+                </h1>
+
+                <Form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Adicionar repositório"
+                        value={newReppo}
+                        onChange={this.handleInputChange}
+                    />
+
+                    <SubmitButton>
+                        <FaPlus color="#FFF" size={14} />
+                    </SubmitButton>
+                </Form>
+            </Container>
+        );
+    }
 }
 
 export default Main;
